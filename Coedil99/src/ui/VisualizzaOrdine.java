f62785d4d2304705734af727e1b55ae46e1cab77
igodip <igor.di.paolo@gmail.com>
2013-10-23 12:39:12 +0200

    Aggiunto visualizzaOrdine

--- Coedil99/src/ui/VisualizzaOrdine.java
+++ Coedil99/src/ui/VisualizzaOrdine.java
@@ -1,12 +1,12 @@
 package ui;
 
  
+import java.io.IOException;
+
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
  
 public class VisualizzaOrdine extends Application {
@@ -15,21 +15,21 @@ public class VisualizzaOrdine extends Application {
     }
     
     @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("Hello World!");
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
- 
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
-        primaryStage.show();
+    public void start(Stage stage) {
+    	Parent root;
+		try {
+			root = FXMLLoader.load(getClass().getResource("visualizza_ordine.fxml"));
+			Scene scene = new Scene(root, 300, 275);
+			 stage.setTitle("FXML Welcome");
+			 stage.setScene(scene);
+			 stage.show();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+    	
+
     }
 }

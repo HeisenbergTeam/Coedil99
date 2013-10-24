551566490ecaec43687646eaf558ad194b36ad0c
filippotp <filippotp@gmail.com>
2013-10-24 13:49:05 +0200

    Prove DAO nel Main, implementati alcuni metodi in Ordine, Commessa e Distinta

--- Coedil99/src/Main.java
+++ Coedil99/src/Main.java
@@ -1,12 +1,67 @@
+import java.util.Date;
+
+import modello_di_dominio.Commessa;
+import modello_di_dominio.DAOFactory;
+import modello_di_dominio.Distinta;
+import modello_di_dominio.Ordine;
+
+import org.orm.PersistentException;
+
 import ui.VisualizzaOrdine;
 
-
 public class Main {
-	
-	public static void main(String args[]){
+
+	@SuppressWarnings("deprecation")
+	public static void main(String args[]) {
 		System.out.println("Start");
-		
+
 		VisualizzaOrdine.start(args);
+
+		Date date = new Date();
+		Ordine ordine = new Ordine();
+
+		ordine.setDataCreazione(date);
+		ordine.creaCommessa();
+		Commessa commessa = ordine.getCommesse().toArray()[0];
+		commessa.setDataCreazione(date);
+		commessa.creaDistinta();
+		Distinta distinta = commessa.getDistinta();
+		distinta.setDataInizio(date);
+
+		try {
+			DAOFactory.getDAOFactory().getOrdineDAO().save(ordine);
+		} catch (PersistentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		try {
+			System.out.print(DAOFactory.getDAOFactory().getOrdineDAO()
+					.getOrdineByORMID(ordine.getID()).getDataCreazione()
+					.toString() + "\n");
+		} catch (PersistentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		try {
+			System.out.print(DAOFactory.getDAOFactory().getCommessaDAO()
+					.getCommessaByORMID(commessa.getID()).getDataCreazione()
+					.toString() + "\n");
+		} catch (PersistentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		try {
+			System.out.print(DAOFactory.getDAOFactory().getCommessaDAO()
+					.getCommessaByORMID(commessa.getID()).getDataCreazione()
+					.toString() + "\n");
+		} catch (PersistentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
-	
+
 }
--- Coedil99/src/modello_di_dominio/Ordine.java
+++ Coedil99/src/modello_di_dominio/Ordine.java
@@ -18,65 +18,76 @@ import java.util.Date;
 public class Ordine {
 	public Ordine() {
 	}
-	
-	private java.util.Set this_getSet (int key) {
+
+	private java.util.Set this_getSet(int key) {
 		if (key == modello_di_dominio.ORMConstants.KEY_ORDINE_COMMESSE) {
 			return ORM_commesse;
 		}
-		
+
 		return null;
 	}
-	
+
 	org.orm.util.ORMAdapter _ormAdapter = new org.orm.util.AbstractORMAdapter() {
 		public java.util.Set getSet(int key) {
 			return this_getSet(key);
 		}
-		
+
 	};
-	
+
 	private int ID;
-	
+
 	private Date dataCreazione;
-	
+
 	private java.util.Set ORM_commesse = new java.util.HashSet();
-	
+
+	public final modello_di_dominio.CommessaSetCollection commesse = new modello_di_dominio.CommessaSetCollection(
+			this, _ormAdapter,
+			modello_di_dominio.ORMConstants.KEY_ORDINE_COMMESSE,
+			modello_di_dominio.ORMConstants.KEY_COMMESSA_ORDINE,
+			modello_di_dominio.ORMConstants.KEY_MUL_ONE_TO_MANY);
+
 	private void setID(int value) {
 		this.ID = value;
 	}
-	
+
 	public int getID() {
 		return ID;
 	}
-	
+
 	public int getORMID() {
 		return getID();
 	}
-	
+
 	public void setDataCreazione(Date value) {
 		this.dataCreazione = value;
 	}
-	
+
 	public Date getDataCreazione() {
 		return dataCreazione;
 	}
-	
+
 	private void setORM_Commesse(java.util.Set value) {
 		this.ORM_commesse = value;
 	}
-	
+
 	private java.util.Set getORM_Commesse() {
 		return ORM_commesse;
 	}
-	
-	public final modello_di_dominio.CommessaSetCollection commesse = new modello_di_dominio.CommessaSetCollection(this, _ormAdapter, modello_di_dominio.ORMConstants.KEY_ORDINE_COMMESSE, modello_di_dominio.ORMConstants.KEY_COMMESSA_ORDINE, modello_di_dominio.ORMConstants.KEY_MUL_ONE_TO_MANY);
-	
+
 	public modello_di_dominio.Commessa creaCommessa() {
-		//TODO: Implement Method
-		throw new UnsupportedOperationException();
+		// TODO: Implement Method
+		this.commesse.add(DAOFactory.getDAOFactory().getCommessaDAO()
+				.createCommessa());
+		return this.commesse.toArray()[commesse.size()-1];
+//		throw new UnsupportedOperationException();
 	}
-	
+
+	public CommessaSetCollection getCommesse() {
+		return this.commesse;
+	}
+
 	public String toString() {
 		return String.valueOf(getID());
 	}
-	
+
 }

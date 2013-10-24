980628e1a5120056f501ff630799b30774102de6
igodip <igor.di.paolo@gmail.com>
2013-10-24 12:25:52 +0200

    :D

--- Coedil99/src/controller/VisualizzaOrdineController.java
+++ Coedil99/src/controller/VisualizzaOrdineController.java
@@ -4,11 +4,14 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.text.Text;
 
-public class FXMLExampleController {
+public class VisualizzaOrdineController {
     @FXML private Text actiontarget;
     
     @FXML protected void handleSubmitButtonAction(ActionEvent event) {
         actiontarget.setText("Sign in button pressed");
     }
+    
+    
+    
 
 }
--- Coedil99/src/ui/VisualizzaOrdine.java
+++ Coedil99/src/ui/VisualizzaOrdine.java
@@ -7,6 +7,9 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
  
 public class VisualizzaOrdine extends Application {
@@ -18,11 +21,21 @@ public class VisualizzaOrdine extends Application {
     public void start(Stage stage) {
     	Parent root;
 		try {
-			root = FXMLLoader.load(getClass().getResource("layout_pagina.fxml"));
+			root = (BorderPane) FXMLLoader.load(getClass().getResource("layout_pagina.fxml"));
+			
 			Scene scene = new Scene(root, 300, 275);
-			 stage.setTitle("FXML Welcome");
-			 stage.setScene(scene);
-			 stage.show();
+			
+			TreeView<String> treeViewOrdini = (TreeView<String>) root.lookup("#treeview_ordini");
+			System.out.println(treeViewOrdini.getClass().toString());
+			
+			
+			stage.setTitle("FXML Welcome");
+			stage.setScene(scene);
+			stage.show();
+			
+			if(scene.lookup("#treeview_ordini") == null){
+				System.out.println("i");
+			}
 			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block

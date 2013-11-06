9fb5139fd5816686a73592ea6b889f0d9986fd0a
filippotp <filippotp@gmail.com>
2013-11-06 12:31:52 +0100

    Aggiunte Ottimizzatore e OttimizzatorePrototipo

---
+++ Coedil99/src/servizi/Ottimizzatore.java
@@ -0,0 +1,9 @@
+package servizi;
+
+import modello_di_dominio.Distinta;
+
+public interface Ottimizzatore {
+	
+	public Distinta Ottimizza(Distinta distinta);
+
+}
---
+++ Coedil99/src/servizi/impl/OttimizzatorePrototipo.java
@@ -0,0 +1,12 @@
+package servizi.impl;
+
+import modello_di_dominio.Distinta;
+import servizi.Ottimizzatore;
+
+public class OttimizzatorePrototipo implements Ottimizzatore {
+	
+	public Distinta Ottimizza(Distinta distinta) {
+		return null;
+	}
+
+}

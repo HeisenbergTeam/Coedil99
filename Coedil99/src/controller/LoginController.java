d101f05a5d5593ebc197a02a6a5c4bc7e9d883de
igodip <igor.di.paolo@gmail.com>
2013-11-06 13:50:51 +0100

    Inserito Pannello di controllo

--- Coedil99/src/controller/LoginController.java
+++ Coedil99/src/controller/LoginController.java
@@ -1,13 +1,18 @@
 package controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import servizi.Autenticazione;
 import servizi.GestoreServizi;
 
@@ -29,7 +34,24 @@ public class LoginController implements Initializable {
 		Autenticazione autenticazione = (Autenticazione) gs.getServizio("AutenticazionePrototipo");
 		
 		if(autenticazione.login(username.getText(),password.getText())){
-			System.out.println("ciao");
+			Stage stage = (Stage) username.getScene().getWindow();
+			stage.close();
+			
+			Parent root = null;
+			try {
+				root = FXMLLoader.load(getClass().getResource("../ui/fxml/pannello_di_controllo.fxml"));
+				Scene scene = new Scene(root, 640, 480);
+				
+				Stage s = new Stage();
+				s.setTitle("FXML Welcome");
+				s.setScene(scene);
+				s.show();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			
 		}
 	}
 }

d5bcdb59aa63cc396576d3a12fb36492bd8b2298
filippotp <filippotp@gmail.com>
2013-11-06 12:42:27 +0100

    Aggiunta AutenticazionePrototipo, modificata Autenticazione

---
+++ Coedil99/src/servizi/impl/AutenticazionePrototipo.java
@@ -0,0 +1,11 @@
+package servizi.impl;
+
+import servizi.Autenticazione;
+
+public class AutenticazionePrototipo implements Autenticazione {
+	
+	public boolean login(String username, String password) {
+		return true;
+	}
+
+}
